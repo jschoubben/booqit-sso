95e73e2a7297352913245e063a24c5ebc94547da
jschoubben <jschoubben@gmail.com>
2018-11-27 19:08:23 +0100

    Finished SSO demo

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+  "development": {
+    "appName": "Booqit SSO - Dev",
+    "booqitApi": "http://localhost:4200/",
+    "apiKey": "419c447645842f2bb1db91753bc40749c7dca6039f5ef5a0e321c3ea38fb8dc7"
+  },
+
+  "test": {
+    "appName": "Booqit SSO - Test",
+    "booqitApi": "https://test.booqitapp.com/"
+  },
+
+  "staging": {
+    "appName": "Booqit SSO - Staging",
+    "booqitApi": "https://staging.booqitapp.com/"
+  },
+
+  "production": {
+    "appName": "Booqit SSO",
+    "booqitApi": "https://booqitapp.com/"
+  }
+
+};
--- server.js
+++ server.js
@@ -1,13 +1,45 @@
-var 
+var
   express = require('express'),
+  request = require('request'),
   http = require('http');
 
 var app = express();
 
 app.use(express.static('./'));
+global.config = require('./config')[app.get('env')]; // Environment config
 
 app.get('/sso', function (req, res) {
-  res.send('We zijn er!!!!');
+  request({
+    url: `${config.booqitApi}api/v1/sso/generate-link`,
+    method: 'GET',
+    headers: {
+      authorization: `key ${config.apiKey}`,
+    },
+    json: {
+      firstName: 'Lily',
+      lastName: 'Doe',
+      internalNumber: '123456',
+      roles: {
+        user: true
+      }, // Or admin: true
+      language: 'en' // Or 'nl', 'fr'
+    }
+  }, (err, responseObj, body) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    if (responseObj.statusCode !== 200) {
+      return res.status(500).json(body || 'Invalid request');
+    }
+    if (responseObj.statusCode !== 200 || !body) {
+      return res.status(responseObj.statusCode).json(body);
+    }
+    if (body.data.ssoUrl) {
+      return res.redirect(body.data.ssoUrl);
+    } else {
+      return res.status(200).json(body);
+    }
+  });
 });
 
 const server = http.createServer(app);
